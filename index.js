6ab1f7daeaacfd3fde7b96263a14797fa4e2ca5b
Geniye1 <jstrayer57@gmail.com>
2023-09-14 19:46:15 -0500

    Completely rebuilt code from ground up - is basically in the same state as it was in the prior commit the logic is just different

--- index.js
+++ index.js
@@ -1,78 +1,3 @@
-function operate(currentOperator) {
-    // If result is not zero then that means we are in the middle of an expression and should use result as
-    // the first term in the calculation instead of num1 so the expression can be calculated for as long
-    // as the user needs
-    switch (cachedOperator) {
-        case "+":
-            result = (result == 0? num1 + num2 : result + num2);
-            break;
-        case "-":
-            result = (result == 0? num1 - num2 : result - num2);
-            break;
-        case "X":
-            result = (result == 0? num1 * num2 : result * num2);
-            break;
-        case "÷":
-            result = (result == 0? num1 / num2 : result / num2);
-            break;
-    }
-
-    // End of expression, reset calculator
-    if (currentOperator === "=") {
-        num1 = undefined;
-        num2 = undefined;
-        cachedOperator = undefined;
-        
-        // Display result, then reset it 
-        displayResult(result);
-        result = 0;
-    }
-    else {
-        // Reset num2 and cache the operator the user clicked so that the code can cycle back and continue receiving inputs
-        num2 = undefined;
-        cachedOperator = currentOperator;
-        displayResult(result);
-    }
-}
-
-function displayResult(a) {
-    outputText.textContent = a;
-}
-
-const outputText = document.querySelector("#output-text");
-
-let num1 = undefined;
-let num2 = undefined;
-let result = 0;
-let cachedOperator = undefined;
-
-// The buttons wait for the num2 to become defined, upon which it will call operate() then reset num2 until = is hit
-const buttons = document.querySelectorAll(".calculator-button");
-buttons.forEach(button => {
-    button.addEventListener("click", function() {
-        // Attempt to parse the data-value to an int, if it is successfully parsed
-        // then it is a digit. Otherwise, it is an operator.
-        let parsedDataValue = parseInt(button.dataset.value);
-        if (!isNaN(parsedDataValue)) {
-            if (num1 == undefined) {
-                num1 = parsedDataValue;
-            }
-            else if (num2 == undefined) {
-                num2 = parsedDataValue;
-            }
-            displayResult(parsedDataValue);
-        } 
-        else {
-            if (num2 == undefined) {
-                cachedOperator = button.dataset.value.toString();
-            } 
-            else {
-                operate(button.dataset.value.toString());
-            }
-        }
-    })
-});
-
 /*
     Flow of code:
     1. Click digit, populate num1 with this value and push it to the output text
@@ -84,3 +9,109 @@ buttons.forEach(button => {
     5. Keep looping until they hit the '=', then reset the calculator (not the output text)
     setting num1, num2, result, and operator back to default values.
 */
+
+function processDigitButtonEvent(digitButton) {
+    let currentDigit = digitButton.dataset.value;
+    currentNumberString += currentDigit;
+    displayToScreen(currentNumberString);
+}
+
+// Process the operation
+// This is operate() im just not calling it that so it matches the pattern of the above function's name
+function processOperatorButtonEvent(currentOperatorButton) {
+
+    // Regardless of the operator, numberString needs to be passed to a, b, or c
+    updateNumbers();
+
+    // If this is the first operation, all it needs to do is cache it then move on
+    if (cachedOperator == undefined) {
+        cachedOperator = currentOperatorButton.dataset.value.toString();
+        return;
+    }
+    
+    // If an operator HAS been cached then it is time to perform a calculation using it (for now disregarding the operator
+    // the user has clicked on currently)
+    switch (cachedOperator) {
+        case "+":
+            result = (result == 0? a + b : result + c);
+            console.log("a: " + a + ", b: " + b + ", result: " + result);
+            break;
+        case "-":
+            result = (result == 0? a - b : result - c);
+            break;
+        case "X":
+            result = (result == 0? a * b : result * c);
+            break;
+        case "÷":
+            result = (result == 0? a / b : result / c);
+            break;
+    }
+
+    // Now, get the operator the user clicked on this time and determine whether or not we should end the
+    // expression. If so (i.e. operator is '='), then display the final result and reset the calculator to it's default state
+    // If the user clicks any other operator (other than clear), then it shoudl merely cache the result so it can be used
+    // in the next operation.
+    let currentOperatorValue = currentOperatorButton.dataset.value.toString();
+    if (currentOperatorValue == "=") {
+        a = b = c = cachedOperator = undefined;
+        currentNumberString = "";
+        displayToScreen(result);
+        result = 0;
+        return;
+    }
+    else if (currentOperatorValue == "C") {
+        a = b = c = cachedOperator = undefined;
+        result = 0;
+        currentNumberString = "";
+        displayToScreen("0");
+        return;
+    }
+    else
+    {
+        cachedOperator = currentOperatorValue;
+        updateNumbers();
+    }
+    displayToScreen(result);
+}
+
+function displayToScreen(value) {
+    outputText.textContent = value.toString();
+}
+
+// This function will handle the passing of the typed string to the internal variables used for calculations
+function updateNumbers() {
+    if (a == undefined)      a = parseFloat(currentNumberString);
+    else if (b == undefined) b = parseFloat(currentNumberString);
+    else                     console.log("SETTING C"); c = parseFloat(currentNumberString);
+
+    currentNumberString = "";
+}
+
+// 'a' and 'b' are the first two digits in the expression, 'c' is a placeholder for all extra values that 
+// are entered afterwards and if used to operate on result - which is obtained after the operation between a and b are calculated
+let a = b = c = undefined;
+let result = 0;
+let cachedOperator = undefined;
+
+// Variable that will be used to build the number that will eventually be passed to a, b, or c. It allows me to
+// type in any n-length number to the calculator.
+let currentNumberString = "";
+
+const outputText = document.querySelector("#output-text");
+
+const digits = document.querySelectorAll(".digit");
+const operators = document.querySelectorAll(".operator");
+
+digits.forEach(digitButton => {
+    digitButton.addEventListener("click", (e) => {
+        processDigitButtonEvent(e.target);
+    });
+});
+
+operators.forEach(operatorbutton => {
+    operatorbutton.addEventListener("click", (e) => {
+        processOperatorButtonEvent(e.target);
+    });
+});
+
+
